415335c3fa603110263c14a8c7c6d296467f0d84
Oking123 <35648547+Oking123@users.noreply.github.com>
2020-11-23 17:18:05 -0500

    modified version of IgblastMatch

--- src/com/rapidnovor/BlastResult.java
+++ src/com/rapidnovor/BlastResult.java
@@ -1,14 +1,23 @@
 package com.rapidnovor;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.List;
 
 /**
- * @description: Created by Zixuan Jin on 2020-11-23.
+ * @description: Object to store the result of IgBLAST
+ * Created by Zixuan Jin on 2020-11-23.
  */
 public class BlastResult {
     private String query;
     private String querySpecies;
-    private String version = "v1.0";
+    private final String version = "v1.0";
     private List<IgblastMatch> matches;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+    @Override
+    public String toString() {
+        return gson.toJson(this);
+    }
 }
--- src/com/rapidnovor/IgblastMatch.java
+++ src/com/rapidnovor/IgblastMatch.java
@@ -1,11 +1,12 @@
 package com.rapidnovor;
 
 /**
- * @description:
+ * @description: Object to store one single match result
  * Created by Zixuan Jin on 2020-11-20.
  */
 public class IgblastMatch {
-    private class Detail{
+
+    private static class Detail{
         private String title;
         private double matchRatio;
         private String species;
@@ -13,11 +14,19 @@ public class IgblastMatch {
         private int queryTail;
         private int matchHead;
         private int matchTail;
-        Detail(String title, String species, double matchRatio){
+
+        public void setTitle(String title) {
             this.title = title;
-            this.species = species;
+        }
+
+        public void setMatchRatio(double matchRatio) {
             this.matchRatio = matchRatio;
         }
+
+        public void setSpecies(String species) {
+            this.species = species;
+        }
+
         public void SetQueryIndex(int head, int tail){
             this.queryHead = head;
             this.queryTail = tail;
@@ -28,7 +37,65 @@ public class IgblastMatch {
             this.matchTail = tail;
         }
 
+        public String getTitle() {
+            return title;
+        }
 
+        public double getMatchRatio() {
+            return matchRatio;
+        }
+
+        public String getSpecies() {
+            return species;
+        }
+
+        public int getQueryHead() {
+            return queryHead;
+        }
+
+        public int getQueryTail() {
+            return queryTail;
+        }
+
+        public int getMatchHead() {
+            return matchHead;
+        }
+
+        public int getMatchTail() {
+            return matchTail;
+        }
     }
+
+    private static class Alignment{
+
+        private final String query;
+
+        private final String match;
+
+        Alignment(String query, String match){
+            this.query = query;
+            this.match = match;
+        }
+
+        public String getQuery() {
+            return query;
+        }
+
+        public String getMatch() {
+            return match;
+        }
+    }
+
+    private String match;
+
+    private int rank;
+
+    private Detail detail;
+
+    private Alignment alignment;
+
+
+
+
 
 }
